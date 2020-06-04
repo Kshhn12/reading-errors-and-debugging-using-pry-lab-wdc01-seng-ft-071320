0c693d13d476def3ed94d77f54870a2087766c5d
Kevin Sheehan <kshhn12@gmail.com>
2020-06-04 12:01:17 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,8 +1,9 @@
 # don't forget to add: require 'pry'
 
 def snake_it_up(string)
-  for(Int i = 0; string[0] == "s" && i < 9; i++)
-    "s" + string
+  for(Int i = 0; string[0] == "s" && i < 9; i++){
+   "s" + string 
+  }
   else
     string
   end
